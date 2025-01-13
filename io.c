697f9dfc1264676cf1783e872f58aef700fe67b4
Shaamallow <eyal.benaroche@gmail.com>
2025-01-13 12:08:06 +0100

    remove copy and formatting

--- io.c
+++ io.c
@@ -1,8 +1,8 @@
+#include "transfo.h"
 #include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "transfo.h"
 #ifdef USE_CLOCK
 #include <time.h>
 #else
@@ -15,65 +15,66 @@ clock_t transform_image(char *source, char *curve, int light, char *dest)
 double transform_image(char *source, char *curve, int light, char *dest)
 #endif
 {
-    FILE *in = fopen(source, "r");
-    FILE *map = fopen(curve, "r");
-    if (!in || !map) exit(1);
+  FILE *in = fopen(source, "r");
+  FILE *map = fopen(curve, "r");
+  if (!in || !map)
+    exit(1);
 
-    int height, width, maxval;
-    fscanf(in, "P5\n%d %d\n%d\n", &height, &width, &maxval);
-    if (maxval > 255) exit(1);
+  int height, width, maxval;
+  fscanf(in, "P5\n%d %d\n%d\n", &height, &width, &maxval);
+  if (maxval > 255)
+    exit(1);
 
-    long size = width * height;
-    unsigned char *source_image = malloc(size);
-    unsigned char *dest_image = malloc(size);
-    unsigned char lut[256];
+  long size = width * height;
+  unsigned char *source_image = malloc(size);
+  unsigned char lut[256];
 
-    fread(lut, 1, 256, map);
-    fread(source_image, 1, size, in);
+  fread(lut, 1, 256, map);
+  fread(source_image, 1, size, in);
 
-    fclose(in);
-    fclose(map);
+  fclose(in);
+  fclose(map);
 
 #ifdef USE_CLOCK
-    clock_t start = clock();
+  clock_t start = clock();
 #else
-    start_counter();
+  start_counter();
 #endif
-    transfo(width, height, source_image, dest_image, lut, light);
+  transfo(width, height, source_image, lut, light);
 #ifdef USE_CLOCK
-    clock_t t = clock() - start;
-    printf("%ld clock cycles.\n", t);
+  clock_t t = clock() - start;
+  printf("%ld clock cycles.\n", t);
 #else
-    double t = get_counter();
-    printf("%f clock cycles.\n", t);
+  double t = get_counter();
+  printf("%f clock cycles.\n", t);
 #endif
 
-    FILE *out = fopen(dest, "w");
-    fprintf(out, "P5\n%d %d\n%d\n", height, width, maxval);
-    fwrite(dest_image, 1, size, out);
-    fclose(out);
+  FILE *out = fopen(dest, "w");
+  fprintf(out, "P5\n%d %d\n%d\n", height, width, maxval);
+  fwrite(source_image, 1, size, out);
+  fclose(out);
 
-    free(source_image);
-    free(dest_image);
-    return t;
+  free(source_image);
+  return t;
 }
 
 void run_transfo_file(FILE *tf) {
-    char source[255], curve[255], dest[255];
-    int light;
-    while (fscanf(tf, "%s %s %d %s", source, curve, &light, dest) == 4) {
-        transform_image(source, curve, light, dest);
-    }
+  char source[255], curve[255], dest[255];
+  int light;
+  while (fscanf(tf, "%s %s %d %s", source, curve, &light, dest) == 4) {
+    transform_image(source, curve, light, dest);
+  }
 }
 
 int main(int ac, char *av[]) {
-    if (ac != 2) exit(1);
-    FILE *tf = fopen(av[1], "r");
-    if (!tf) exit(1);
+  if (ac != 2)
+    exit(1);
+  FILE *tf = fopen(av[1], "r");
+  if (!tf)
+    exit(1);
 
-    chdir(dirname(av[1]));
-    run_transfo_file(tf);
-    fclose(tf);
-    return 0;
+  chdir(dirname(av[1]));
+  run_transfo_file(tf);
+  fclose(tf);
+  return 0;
 }
-
--- transfo.c
+++ transfo.c
@@ -32,9 +32,9 @@ void curve(int w, int h, unsigned char *img, unsigned char *lut) {
   }
 }
 
-void transfo(int w, int h, unsigned char *src, unsigned char *dest,
-             unsigned char *lut, unsigned char val) {
-  copy(w, h, src, dest);
-  curve(w, h, dest, lut);
-  light(w, h, dest, val);
+void transfo(int w, int h, unsigned char *src, unsigned char *lut,
+             unsigned char val) {
+  /* copy(w, h, src, dest); */
+  curve(w, h, src, lut);
+  light(w, h, src, val);
 }
